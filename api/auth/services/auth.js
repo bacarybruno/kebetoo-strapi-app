449ce9ce82f1454aa3f6883331d0260482e25f7a
BODIAN Bacary Bruno <bacarybruno@gmail.com>
2020-12-26 23:42:52 +0100

    fix: allow requests with raw token

--- api/auth/services/auth.js
+++ api/auth/services/auth.js
@@ -7,11 +7,11 @@ module.exports = {
     const headerToken = ctx.request && ctx.request.header && ctx.request.header.authorization;
     if (headerToken) {
       try {
-        const idToken = headerToken.split(' ')[1];
+        const idToken = headerToken.replace('Bearer', '').trim();
         const decodedToken = await firebaseAdmin.auth().verifyIdToken(idToken);
         return { ...decodedToken };
       } catch (error) {
-        throw new Error('The specified token is invalid.' + error.message);
+        throw new Error('The specified token is invalid.');
       }
     }
     throw new Error('Missing authorization in header.');
