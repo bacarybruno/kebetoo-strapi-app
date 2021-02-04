e36a1620d1b95db06d802aaf1b61b50c1d8f95cc
BODIAN Bacary Bruno <bacarybruno@gmail.com>
2021-02-04 02:20:07 +0100

    feat(posts): cascade delete posts

--- api/comments/models/comments.js
+++ api/comments/models/comments.js
@@ -5,4 +5,35 @@
  * to customize this model
  */
 
-module.exports = {};
+const deleteContent = (name) => (content) => strapi.services[name].delete({ id: content.id });
+
+const deleteContents = (contents, name) => {
+  if (contents && contents.length > 0) {
+    return contents.map(deleteContent(name))
+  }
+  return []
+};
+
+const deleteAsset = async (asset) => {
+  if (asset) {
+    return strapi.plugins.upload.services.upload.remove(asset);
+  }
+  return Promise.resolve(null);
+};
+
+module.exports = {
+  lifecycles: {
+    async afterDelete({ replies, reactions, audio }) {
+      try {
+        // delete replies
+        await Promise.all(deleteContents(replies, 'comments'));
+        // delete reactions
+        await Promise.all(deleteContents(reactions, 'reactions'));
+        // delete audio
+        await deleteAsset(audio);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+};
